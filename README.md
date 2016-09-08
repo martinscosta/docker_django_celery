f1cdceb76ff2dd7a1a9b1b538692e0ffdc55ee93
Marco Aurelio Martins Costa <martinscosta@gmail.com>
2016-09-07 21:43:22 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Baseado no tutorial 
+
+Tutorial: Celery en tiempo real con Socket.io
+
+Harvey David Forero
+
+https://www.youtube.com/watch?v=cr0cbJ2PkJY 
---
+++ django/webapp/realtime/views.py
@@ -0,0 +1,13 @@
+import time
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from .tasks import generate_csv
+
+class CeleryRealTimeTemplateView(TemplateView):
+    template_name = 'realtime.html'
+
+    def post(self, request, *args, **kwargs):
+        #time.sleep(10)
+        task_id = generate_csv.delay()
+        return HttpResponse(task_id)
